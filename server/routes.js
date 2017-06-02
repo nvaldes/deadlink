202f9eb046fa3eecdc0763ef2e311688268576a5
Valdes, Noriel <nvaldes@c21stores.com>
2017-06-02 13:09:39 -0400

    created server proxy for WL api

--- server/routes.js
+++ server/routes.js
@@ -8,10 +8,17 @@ import errors from './components/errors';
 import path from 'path';
 
 export default function(app) {
+  var proxy = require('http-proxy-middleware');
   // Insert routes below
-  app.use('/api/things', require('./api/thing'));
+  app.use('/api', proxy({
+    target: "https://www.staging.c21.weblinc.com",
+    changeOrigin: true,
+    headers: {
+      "X-WEBLINCTOKEN": "362d77d05bca2fcdeb6a35f11fccc1b5"
+    }
+  }));
   // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+  app.route('/:url(auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
 
   // All other routes should redirect to the index.html
